c7d59ee07717e6a65bd1c70ffe46989fb8168e40
Bennici <alexandre-ben@hotmail.fr>
2023-06-09 16:38:11 +0200

    #1 add time rules

--- R/vrule_threshold.R
+++ R/vrule_threshold.R
@@ -48,6 +48,24 @@ vrule_threshold <- R6Class("vrule_threshold",
  )
 )
 
+#' vrule_date_threshold
+#' @name vrule_date_threshold
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+vrule_date_threshold <- R6Class("vrule_date_threshold",
+                           inherit = vrule_operator_relational,
+                           public = list(
+                             initialize = function(operator, threshold){
+                               super$initialize(operator = operator, expr = as.Date(threshold))
+                             },
+                             
+                             validate = function(value){
+                               super$validate(as.Date(value))
+                             }
+                           )
+)
+
 #' vrule_min
 #' @name vrule_min
 #' @docType class
@@ -64,6 +82,24 @@ vrule_min <- R6Class("vrule_min",
        super$validate(value)
      }
    )
+)
+
+#' vrule_date_min
+#' @name vrule_date_min
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+vrule_date_min <- R6Class("vrule_date_min",
+                     inherit = vrule_operator_relational,
+                     public = list(
+                       initialize = function(minValue){
+                         super$initialize(operator = ">=", expr = as.Date(minValue))
+                       },
+                       
+                       validate = function(value){
+                         super$validate(as.Date(value))
+                       }
+                     )
 )
 
 #' vrule_max
@@ -84,19 +120,65 @@ vrule_max <- R6Class("vrule_max",
    )
 )
 
+#' vrule_date_max
+#' @name vrule_date_max
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+vrule_date_max <- R6Class("vrule_date_max",
+                     inherit = vrule_operator_relational,
+                     public = list(
+                       initialize = function(maxValue){
+                         super$initialize(operator = "<=", expr = as.Date(maxValue))
+                       },
+                       
+                       validate = function(value){
+                         super$validate(as.Date(value))
+                       }
+                     )
+)
+
 #' vrule_range
 #' @name vrule_range
 #' @docType class
 #' @importFrom R6 R6Class
 #' @export
 vrule_range <- R6Class("vrule_range",
+                       inherit = vrule_abstract_simple,
+                       public = list(
+                         minValue = NA,
+                         maxValue = NA,
+                         initialize = function(minValue, maxValue){
+                           self$minValue = minValue
+                           self$maxValue = maxValue
+                         },
+                         
+                         validate = function(value){
+                           range_rule = vrule_operator_and$new(
+                             vrule_min$new(minValue = self$minValue),
+                             vrule_max$new(maxValue = self$maxValue)
+                           )
+                           report = range_rule$validate(value)
+                           return(report)
+                         }
+                       )
+)
+
+
+
+#' vrule_date_range
+#' @name vrule_date_range
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+vrule_date_range <- R6Class("vrule_date_range",
    inherit = vrule_abstract_simple,
    public = list(
      minValue = NA,
      maxValue = NA,
      initialize = function(minValue, maxValue){
-        self$minValue = minValue
-        self$maxValue = maxValue
+        self$minValue = as.Date(minValue)
+        self$maxValue = as.Date(maxValue)
      },
      
      validate = function(value){
@@ -104,7 +186,7 @@ vrule_range <- R6Class("vrule_range",
          vrule_min$new(minValue = self$minValue),
          vrule_max$new(maxValue = self$maxValue)
        )
-       report = range_rule$validate(value)
+       report = range_rule$validate(as.Date(value))
        return(report)
      }
    )
