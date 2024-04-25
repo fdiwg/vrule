2b6cd32fbbe9422c8332a5c3add6b49128d44f66
eblondel <emmanuel.blondel1@gmail.com>
2024-04-25 08:46:09 +0200

    deactivate scientific notation to prevent false  errors with numerics

--- R/profile.R
+++ R/profile.R
@@ -1,4 +1,8 @@
 .onLoad <- function (libname, pkgname) { # nocov start
+  
+  #deactivate scientific notation
+  options(scipen=999)
+  
   #hidden objects
   assign(".vrule", new.env(), envir= asNamespace(pkgname))
   assign(".vrule.options", new.env(), envir= asNamespace(pkgname))
