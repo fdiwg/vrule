cb6785d2b91adaaf47fd130f788acba551c8081a
eblondel <emmanuel.blondel1@gmail.com>
2024-04-25 15:16:28 +0200

    add unit test for vrule_mapping

---
+++ tests/testthat/test_vrule_mapping.R
@@ -0,0 +1,19 @@
+# test_vrule_mapping.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for vrule_cross_column.R
+#=======================
+require(vrule, quietly = TRUE)
+require(testthat)
+
+context("vrule_mapping")
+
+test_that("vrule_mapping",{
+  map = vrule_mapping$new(
+    ref_data_url = "https://raw.githubusercontent.com/fdiwg/fdi-mappings/main/cross-term/codelist_mapping_source_authority_species.csv",
+    ref_source_term = "species",
+    ref_target_term = "source_authority"    
+  )
+  expect_false(map$validate("SBF", target = "IOTC")$valid)
+  expect_true(map$validate("ALB", target = "ICCAT")$valid)
+})
