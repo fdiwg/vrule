fd7987f2c4417fa3c0004b18b6c6dacc4d0a3631
eblondel <emmanuel.blondel1@gmail.com>
2025-05-12 22:33:40 +0200

    #5 tests

--- tests/testthat/test_format_spec.R
+++ tests/testthat/test_format_spec.R
@@ -17,39 +17,11 @@ test_that("decode format spec",{
 })
 
 test_that("decode format spec - benchmarking",{
-  #simplified
-  data <- readr::read_csv("D:/Documents/CLIENTS/FAO/Projets/WECAFC-FIRMS/formated/WECAFC_task_II_1_DMA_2013_2020_simplified_format,.csv", guess_max = 0)
   format = vrule::format_spec$new(
-    json = jsonlite::read_json("https://raw.githubusercontent.com/fdiwg/fdi-formats/main/cwp_rh_simplified_wecafc_taskII.1.json")
+    json = jsonlite::read_json("https://raw.githubusercontent.com/fdiwg/fdi-formats/main/cwp_rh_generic_gta_taskII.json")
   )
-  expect_is(format, "format_spec")
-  
-  #generic
-  data <- readr::read_csv("D:/Downloads/nominal_catch_iotc_level0.csv", guess_max = 0) %>% as.data.frame()
-  data$measurement = "catch"
-  data$measurement_obs = ""
-  data[1,]$fishing_fleet = "United Arab Emirates"
-  
-  format = vrule::format_spec$new(
-    json = jsonlite::read_json("https://raw.githubusercontent.com/fdiwg/fdi-formats/main/cwp_rh_generic_gta_taskI.json")
-  )
-  format$column_specs[[2]]$rules[[1]]$allow_labels = T
-  
-  data_sample = data[1:20,]
-  report = format$validate(data[1:20,])
-  report = report[report$category != "Data structure",]
-  hst = format$display_as_handsontable(data_sample, report, read_only = F)
-  
-  
-  #benchmarking
-  library(rbenchmark)
-  
-  rbenchmark::benchmark(
-    format$validate(data[1:10000,], method = "rowcol"),
-    format$validate(data[1:10000,], method = "matrix"),
-    format$validate(data[1:10000,], method = "grid"),
-    format$validate(data[1:10000,], method = "grid_mapply"),
-    replications = 10
-  )
-  
+  data = readr::read_csv("D:/Downloads/gta/IOTC/iotc_catch_all_1m_firms_level0_2025-04-22_FIXED_processing_level.csv", guess_max = 0)
+  system.time(test1 <- format$validate(data = data, mode = "pair", parallel = T, mc.cores = 12))
+  system.time(test2 <- format$validate(data = data, mode = "column", parallel = T, mc.cores = 12))
+  waldo::compare(test1, test2)
 })
