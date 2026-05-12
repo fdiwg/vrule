4935ec0c6f12c6fe061a667d7e346b5f3ad1b951
eblondel <emmanuel.blondel1@gmail.com>
2026-05-12 12:01:39 +0200

    fix #19

--- tests/testthat/test_format_spec.R
+++ tests/testthat/test_format_spec.R
@@ -9,11 +9,33 @@ require(jsonlite)
 
 context("format_spec")
 
-test_that("decode format spec",{
+test_that("decode format spec 1",{
   format = vrule::format_spec$new(
     json = jsonlite::read_json("https://raw.githubusercontent.com/fdiwg/vrule/main/inst/extdata/tests/specs/format_spec1.json")
   )
   expect_is(format, "format_spec")
+})
+
+test_that("validate 1 row with format spec1",{
+  format = vrule::format_spec$new(
+    json = jsonlite::read_json("https://raw.githubusercontent.com/fdiwg/vrule/main/inst/extdata/tests/specs/format_spec1.json")
+  )
+  dat = data.frame(
+    source_authority = "IOTC",
+    fishing_fleet = "COM",
+    time_start = "2026-01-01",
+    time_end = "2026-01-31",
+    geographic_identifier = "IOTC_WEST",
+    gear_type = "09.1",
+    species = "ALB",
+    fishing_mode = "FS",
+    measurement = "catch",
+    measurement_type = "NC",
+    measurement_value = 1,
+    measurement_unit = "t",
+    measurement_processing_level = "original_eesample"
+  )
+  format$validate(dat)
 })
 
 test_that("decode format spec - benchmarking",{
